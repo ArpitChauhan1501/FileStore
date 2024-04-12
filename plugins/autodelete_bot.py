b8770bd51a3c1940d21371a41fcb00bd4f6963df
Arpit Chauhan <88385002+ArpitChauhan1501@users.noreply.github.com>
2024-04-12 20:58:36 +0530

    Update autodelete_bot.py

--- plugins/autodelete_bot.py
+++ plugins/autodelete_bot.py
@@ -4,7 +4,7 @@ from bot import Bot
 from config import TG_BOT_TOKEN
 
 # Set your Telegram bot token
-bot = bot.Bot("TG_BOT_TOKEN")
+bot = Bot.Bot("TG_BOT_TOKEN")
 
 # Define the message deletion function
 def delete_messages():

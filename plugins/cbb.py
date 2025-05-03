ac93b1e8ab28e7dc482ff1f82bd23d49f59dcbec
Arpit Chauhan <88385002+ArpitChauhan1501@users.noreply.github.com>
2025-05-03 12:48:12 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Support Group : <a href='https://t.me/grandtheftcoders'>Grand Theft Coders</a>",
+            text = f"<b>○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : <a href='https://t.me/+1tP_r1dRfKI2M2Nl'>All Courses</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [

dd10217efdeb4a7a1f9c902c883d32da445af9bb
Arpit Chauhan <88385002+ArpitChauhan1501@users.noreply.github.com>
2025-05-03 12:49:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Group",
+                "Join Channel",
                 url = client.invitelink)
         ]
     ]
